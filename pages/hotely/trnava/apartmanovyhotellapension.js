732d0a84e7daf650bf593a2f903d6cc0607313eb
simon-ugor <simiugor@gmail.com>
2021-01-20 22:49:32 +0100

    hotel update test

--- pages/hotely/trnava/apartmanovyhotellapension.js
+++ pages/hotely/trnava/apartmanovyhotellapension.js
@@ -9,7 +9,7 @@ const apartmanovyhotellapension = () => {
 			<title>Apartmánový hotel La Pension</title>
             <meta name="description" content="Jeden z našich favoritov v destinácii Trnava. Apartmánový dom La Pension sa nachádza v Trnave. Ponúka ubytovanie s vlastným stravovaním."></meta>
 		</Head>
-		<div className="onas-div">
+		<div className="facilities-div">
 			<h2 className="onas-heading">Apartmánový hotel La Pension</h2>
 			<h2 className="onas-subheading">Trnava</h2>
 			<p className="onas-p">Jeden z našich favoritov v destinácii Trnava. Apartmánový dom La Pension sa nachádza v Trnave. Ponúka ubytovanie s vlastným stravovaním. Využívať môžete bezplatné Wi-Fi pripojenie na internet.V apartmánoch La Pension máte k dispozícii posedenie a káblovú TV, plne vybavenú kuchyňu s mikrovlnnou rúrou a chladničkou ako i súkromnú kúpeľňu so sprchovacím kútom.V apartmánovom dome La Pension sa nachádza bar. K dispozícii máte bezplatné parkovisko.Bratislavské letisko sa nachádza 37 km od apartmánového domu.</p>
